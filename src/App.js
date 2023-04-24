a8934cac1844f2b77f9154be871813c0b81d54ee
ManausK <1905246@kiit.ac.in>
2023-04-24 08:21:10 +0530

    Last few changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+import Todo from "./Todo.js";
+
+export const ACTIONS = {
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo",
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+          return todo;
+        }
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.payload.id);
+    default:
+      return todos;
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
 
-  function increment() {
-    setCount((prevCount) => prevCount + 1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
   }
-  function decrement() {
-    setCount((prevCount) => prevCount - 1);
-  }
+
+  // console.log(todos);
 
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
+      })}
     </>
   );
 }
